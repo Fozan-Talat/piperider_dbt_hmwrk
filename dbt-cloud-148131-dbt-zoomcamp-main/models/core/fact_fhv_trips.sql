fedfd1a2902220206ecc583c4566c6f550348241
Fozan Talat <fozantalat@gmail.com>
2023-03-16 14:06:02 +0500

    first commit

---
+++ dbt-cloud-148131-dbt-zoomcamp-main/models/core/fact_fhv_trips.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='table') }}
+
+with fhv_data as (
+    select *, 
+        'fhv' as service_type 
+    from {{ ref('stg_fhv_tripdata') }}
+), 
+dim_zones as (
+    select * from {{ ref('dim_zones') }}
+    where borough != 'Unknown'
+)
+select fhv_data.* 
+from fhv_data
+inner join dim_zones as pickup_zone
+on fhv_data.PUlocationID = pickup_zone.locationid
+inner join dim_zones as dropoff_zone
+on fhv_data.DOlocationID = dropoff_zone.locationid
